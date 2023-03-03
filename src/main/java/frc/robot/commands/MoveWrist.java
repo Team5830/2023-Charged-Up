5015384be0facdb94dd6f147f1a4c38a9856e610
Aaron Steele <asteele11@yahoo.com>
2023-03-02 21:56:47 -0500

    dont stop wrist PID

--- src/main/java/frc/robot/commands/MoveWrist.java
+++ src/main/java/frc/robot/commands/MoveWrist.java
@@ -28,7 +28,7 @@ public class MoveWrist extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    wrist.Stop();
+    //wrist.Stop();
   }
 
   // Returns true when the command should end.
